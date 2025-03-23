0af8a21998d6f8aa6289b186c8aefc59bc8372bd
Gabriel Bello <johngabrielbello@outlook.com>
2025-03-23 12:29:35 -0700

    Client is able to pick up data.

--- client.py
+++ client.py
@@ -7,14 +7,40 @@ SERVER_PORT = 12345
 
 def start_client():
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect((SERVER_IP, SERVER_PORT))
-  print("Connected to server!") # Debugging purposes
 
-  while True:
-    key = keyboard.read_event().name
-    if key in ["w", "a", "s", "d", "space"]:
-      client.send(key.upper().encode('utf-8'))
-      print(f"Sent: {key.upper()}") # Debugging Purposes
+  try:
+    client.connect((SERVER_IP, SERVER_PORT))
+    server_message = client.recv(1024).decode('utf-8')
+    print(f"Server: {server_message}")
+
+    if "full" in server_message.lower():
+      server_message = client.recv(1024).decode('utf-8')
+      print(f"Server: {server_message}")
+      client.close()
+      return
+    
+    while True:
+    # Send Keyboard inputs
+      key = keyboard.read_event().name
+      if key in ["w", "a", "s", "d", "space"]:
+        client.send(key.upper().encode('utf-8'))
+        print(f"Sent: {key.upper()}") # Debugging Purposes
+
+        # Receieve any addtional information from server after user clicks
+        # Note: Can be used for game logic/map updating? Since server will send back information
+        try:
+           response = client.recv(1024).decode('utf-8')
+           if response:
+              print(f"Server: {response}")
+        except:
+           pass # Ignore errors if there was no msgs
+
+  except ConnectionRefusedError:
+      print("Unable to connect to the server. Make sure the server is running.")
+  except KeyboardInterrupt:
+      print("Client shutting down.")
+  finally:
+      client.close()
 
 if __name__ == "__main__":
   start_client()
