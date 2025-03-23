902234f610bc1704b45ebc4d0c509e6a71fa70c3
Gabriel Bello <johngabrielbello@outlook.com>
2025-03-23 11:57:17 -0700

    Helper files in server.py

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import select
 HOST_IP = '0.0.0.0'
 PORT = 12345
 MAX_CLIENTS = 2
+def print_server_capacity(current, capacity):
+  print(f"Server Capacity: {current} / {capacity}")
+  return
 
 def start_server():
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,8 +27,8 @@ def start_server():
           client_socket, client_address = server.accept()
           print(f"{client_address} has joined the game." )
           clients.append(client_socket)
-          print(f"Server Capacity: {len(clients)} / {MAX_CLIENTS}")
           client_socket.send("Welcome to the server!".encode('utf-8'))
+          print_server_capacity(len(clients), MAX_CLIENTS)
         else:
           client_socket, client_address = server.accept()
           print(f"Connection from {client_address} is declined because server is currently full.")
@@ -36,15 +39,17 @@ def start_server():
           data = sock.recv(1024).decode('utf-8')
           if not data:
             # Disconnect
-            print(f"{socket.getpeername()} has left the game.")
             clients.remove(sock)
             sock.close()
+            print(f"{socket.getpeername()} has left the game.")
+            print_server_capacity(len(clients), MAX_CLIENTS)
           else:
             print(f"Received from {sock.getpeername()}: {data}")
         except ConnectionResetError:
-          print(f"[ERROR] {sock.getpeername()} left the game.")
           clients.remove(sock)
           sock.close()
+          print(f"[ERROR] {sock.getpeername()} left the game.")
+          print_server_capacity(len(clients), MAX_CLIENTS)
 
 if __name__ == "__main__":
     start_server()
