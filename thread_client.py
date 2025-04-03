b483b65759a24b98db1667e7b727ebac72faf9e2
Zach Schuler <zs.gitmail@gmail.com>
2025-04-03 15:54:31 -0700

    began combining backend and frontend logic

--- thread_client.py
+++ thread_client.py
@@ -1,9 +1,10 @@
-# WIP 
+# thread_client.py
 import socket
 import threading
 import pygame
 import json
 import time
+from board_utils import draw_grid_outlines, draw_shape_outlines, reveal_shapes
 
 # Server Details
 SERVER_IP = '127.0.0.1'  # Only to connect to server if on the same machine!
@@ -13,15 +14,33 @@ buffer = "" # Store all incoming data into a buffer
 
 # Pygame Details
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+SCREEN_WIDTH = 1520
+SCREEN_HEIGHT = 960
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Client")
 running = True
 text_font = pygame.font.SysFont("Arial", 30) # Default font to pass into draw_text
+PLAYER_COLOURS = ["red", "blue", "green", "purple", "orange"]
 
 # Store player data globally
 player_data = {"players": []}
 data_lock = threading.Lock()    # To prevent 2 threads from accessing the same data and corrupting it
 server_age = 0
+
+# Board Details
+ROWS, COLS = 7, 9
+CELL_WIDTH = 85
+CELL_HEIGHT = 85
+total_board_width = CELL_WIDTH * COLS
+total_board_height = CELL_HEIGHT * ROWS
+BOARD_OFFSET_X = (SCREEN_WIDTH - total_board_width) // 2
+BOARD_OFFSET_Y = (SCREEN_HEIGHT - total_board_height) // 2
+game_board = None
+
+show_outlines = True
+show_full_shapes = False
+show_grid_outlines = False
+region_revealed = [False, False, False]
 
 def send_data():
     while True:
@@ -48,6 +67,16 @@ def send_data():
                 print(f"Error sending data during select: {e}")
                 break
 
+        if keys[pygame.K_p]:       # "READY"
+            try:
+                data_dict = {"TYPE": "READY"}
+                data = json.dumps(data_dict)
+                client.send(data.encode())
+            except Exception as e:
+                print(f"Error sending data during ready: {e}")
+                break
+
+
         # If input is being recieved
         if direction.length() > 0:
             direction = direction.normalize()
@@ -70,7 +99,7 @@ def send_data():
 
 def receive_data():
     
-    global buffer, player_data, server_age
+    global buffer, player_data, server_age, game_board, show_outlines, show_full_shapes, show_grid_outlines, region_revealed
 
     while True:
         try:
@@ -90,7 +119,17 @@ def receive_data():
                         with data_lock: # Prevents race conditions
                             player_data = dict_data # Store the latest info globally (for draw_players)
                         server_age = dict_data["age"]
+                        show_outlines = dict_data["show_outlines"]
+                        show_full_shapes = dict_data["show_full_shapes"]
+                        show_grid_outlines = dict_data["show_grid_outlines"]
+                        region_revealed = dict_data["region_revealed"]
+
                     
+                    case "START_ROUND":
+                        game_board = dict_data["game_board"]
+                        print(f"Recieved game board from server: {game_board}")
+                        
+
                     case _:
                         print(f"Invalid type: {dict_data['TYPE']}")
                 
@@ -103,17 +142,6 @@ def start_client():
     try:
         client.connect((SERVER_IP, SERVER_PORT))
 
-        '''
-        # Wait for the initial server message after connection
-        server_message = client.recv(1024).decode('utf-8')
-        print(f"Server: {server_message}")
-
-        if "full" in server_message.lower():
-            server_message = client.recv(1024).decode('utf-8')
-            print(f"Server: {server_message}")
-            client.close()
-            return
-        '''
         # Start separate threads for sending and receiving data
         send_thread = threading.Thread(target=send_data, daemon=True)
         receive_thread = threading.Thread(target=receive_data, daemon=True)
@@ -129,11 +157,9 @@ def start_client():
         print("Unable to connect to the server. Make sure the server is running.")
     except KeyboardInterrupt:
         print("Client shutting down.")
-    finally:
-        #client.close()
-        pass
 
 
+# Unused, but just for demonstration of how the packets look
 test_packet = {
     "TYPE": "UPDATE",
     "players": [
@@ -145,16 +171,31 @@ test_packet = {
 def draw_players():
     ''' Draws all the players on the screen, based on the list of player coordinates. '''
 
-    colours = ["red", "blue", "green", "purple", "orange"]
-
     with data_lock: # To prevent 2 threads from accessing the same info at the same time
         for player in player_data["players"]:
-            pygame.draw.circle(screen, colours[player["id"] % len(colours)], (player["x"], player["y"]), 25)
+            pygame.draw.circle(screen, PLAYER_COLOURS[player["id"] % len(PLAYER_COLOURS)], (player["x"], player["y"]), 25)
+
+
+def draw_board():
+    global game_board
+
+    if game_board:
+        reveal_shapes(screen, game_board, ROWS, COLS, CELL_WIDTH, CELL_HEIGHT,
+                      BOARD_OFFSET_X, BOARD_OFFSET_Y, PLAYER_COLOURS, region_revealed)
+        if show_outlines:
+            draw_shape_outlines(screen, game_board, ROWS, COLS, CELL_WIDTH, CELL_HEIGHT,
+                                BOARD_OFFSET_X, BOARD_OFFSET_Y, "white")
+        if show_grid_outlines:
+            draw_grid_outlines(screen, ROWS, COLS, CELL_WIDTH, CELL_HEIGHT,
+                               BOARD_OFFSET_X, BOARD_OFFSET_Y, "white")
+
 
 
 def draw_text(text, font, colour, x, y):
     img = font.render(text, True, colour)
     screen.blit(img, (x, y))
+
+
 
 if __name__ == "__main__":
     start_client()
@@ -169,6 +210,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
 
+        draw_board()
         draw_players()
         draw_text(str(server_age), text_font, (255,255,255), 0, 0)
 
--- thread_server.py
+++ thread_server.py
@@ -1,8 +1,10 @@
+# thread_server.py
 import socket
 import threading
 import json
 import time
 from Player import Player
+from generate_game_board import generate_game_board
 
 # Server Variables/Information
 HOST_IP = '0.0.0.0'
@@ -11,6 +13,28 @@ MAX_CLIENTS = 3
 clients = []  # Hold a list of connected clients
 available_player_ids = set(range(MAX_CLIENTS)) # set of available IDs
 server_start_time = time.time()
+data_lock = threading.Lock() # To prevent 2 threads from accessing the same data and corrupting it
+
+# Board Info (same as on client side)
+SCREEN_WIDTH = 1520
+SCREEN_HEIGHT = 960
+ROWS, COLS = 7, 9
+CELL_WIDTH = 85
+CELL_HEIGHT = 85
+total_board_width = CELL_WIDTH * COLS
+total_board_height = CELL_HEIGHT * ROWS
+BOARD_OFFSET_X = (SCREEN_WIDTH - total_board_width) // 2
+BOARD_OFFSET_Y = (SCREEN_HEIGHT - total_board_height) // 2
+game_board = None
+
+# USE DATA LOCK ON THESE
+show_outlines = True
+show_full_shapes = False
+show_grid_outlines = False
+region_revealed = [False, False, False]
+
+
+
 
 def print_server_capacity(current, capacity):
     print(f"Server Capacity: {current} / {capacity}")
@@ -18,6 +42,25 @@ def print_server_capacity(current, capacity):
 
 def get_server_age():
     return int(time.time() - server_start_time)
+
+def check_ready_status():
+    ''' Checks if all connected players are ready and sends the board if they are. '''
+    global game_board
+
+    players_ready = 0
+    for player in clients:
+        if player.is_ready:
+            players_ready += 1
+    print(f"{players_ready} / {len(clients)} are ready!")
+
+    # If all players are ready
+    if players_ready == len(clients):
+        print("All players are reading. Starting the round!")
+        game_board = generate_game_board(ROWS, COLS)
+
+        message = json.dumps({"TYPE": "START_ROUND", "game_board": game_board}) + "\n"
+        for player in clients:
+            player.client_socket.sendall(message.encode())
 
 def broadcast_positions():
     ''' Broadcasts all player positions to every client at a fixed interval. '''
@@ -27,10 +70,17 @@ def broadcast_positions():
             # Create the list of player positions
             positions = [{"id": player.player_id, "x": player.x_pos, "y": player.y_pos} for player in clients]
 
-            # Convert to json to send to all clients
-            message = json.dumps({"TYPE": "UPDATE",
-                                  "players": positions,
-                                  "age": get_server_age()}) + "\n"
+            message = None
+            with data_lock:
+                # Convert to json to send to all clients
+                message = json.dumps({"TYPE": "UPDATE",
+                                    "players": positions,
+                                    "show_outlines": show_outlines,
+                                    "show_full_shapes": show_full_shapes,
+                                    "show_grid_outlines": show_grid_outlines,
+                                    "region_revealed": region_revealed,
+                                    "age": get_server_age()}) + "\n"
+                
             for player in clients:
                 try:
                     player.client_socket.sendall(message.encode())
@@ -40,6 +90,8 @@ def broadcast_positions():
         time.sleep(0.01) # Send updates every 0.01 seconds
 
 def handle_client(player: Player):
+    global show_full_shapes, show_grid_outlines, show_outlines, region_revealed
+    
     try:
         print(f"{player.client_address} has joined the game.")
         welcome_message = json.dumps({"TYPE": "TEXT", "message": "Welcome to the server!"}) + "\n"
@@ -63,18 +115,38 @@ def handle_client(player: Player):
                     case "MOVE":
                         player.update_position(data_dict['direction_x'], data_dict['direction_y'])
                         print(f"{player.client_address} has moved to: ({player.x_pos}, {player.y_pos})") # For debugging
+                    
                     case "SELECT":
                         if player.has_selected == False:                            
                             x = player.x_pos
                             y = player.y_pos
+                            print(f"{player.client_address} has selected: ({x}, {y})")
 
-                            # TODO: Implement Prottoy's selection logic here
-                        else:
-                            print(f"[ERROR] {player.client_address} tried to SELECT but already has!")
+                            # Prottoy's selection logic
+                            col_selected = int((x - BOARD_OFFSET_X) // CELL_WIDTH)
+                            row_selected = int((y - BOARD_OFFSET_Y) // CELL_HEIGHT)
+
+                            # TODO: Check bounds of selection?
+
+                            selected_region_id = game_board[row_selected][col_selected]
+
+                            with data_lock: # to protect shared data access
+                                if not region_revealed[selected_region_id]:
+                                    region_revealed[selected_region_id] = True # TODO: Assign this to a player rather than true
+
+                                    # TODO: check for winners
+                            
+
+                            player.has_selected = True
+
+                    case "READY":
+                        if player.is_ready == False:
+                            player.is_ready = True
+                            check_ready_status()
+                    
                     case _:
                         print(f"[ERROR] Invalid packet type: {data_dict['TYPE']}")
 
-                #client_socket.send("ACK".encode('utf-8'))
 
             # Regardless, run:
             player.update_position(0,0) # TODO: test this
@@ -88,6 +160,12 @@ def handle_client(player: Player):
         player.client_socket.close()
         print_server_capacity(len(clients), MAX_CLIENTS)
 
+def physics_loop():
+    """ Continuously updates player positions even when no input is received. """
+    while True:
+        for player in clients:
+            player.update_position(0,0)
+        time.sleep(1/60)
 
 def start_server():
     
@@ -99,6 +177,10 @@ def start_server():
     # Start the broadcasting thread
     broadcast_thread = threading.Thread(target=broadcast_positions, daemon=True)
     broadcast_thread.start()
+
+    # Thread to continously run physics. # TODO doesn't really work currently
+    physics_thread = threading.Thread(target=physics_loop, daemon=True)
+    physics_thread.start()
 
     while True:
         if len(clients) < MAX_CLIENTS:
